bf67ba41e4145c7a0c0edcda9ba9cf6257fe4a76
q13117 <carlxie@hotmail.com>
2019-01-04 00:26:00 +0000

    Add golang fixes #1

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X Static Master golang http example(jx import) #1"
+	title := "Jenkins X Static Master golang http example( jx import ) #1"
 
 	from := ""
 	if r.URL != nil {
